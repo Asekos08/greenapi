c3b224ead6f50d7f9e080b1065f35c27c3656d99
Asekos08 <aslanbek.turar@nu.edu.kz>
2024-06-09 02:31:14 +0500

    Third commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 app.add_middleware(
     CORSMiddleware,
